4168f93e8f12957900881035ce6f1f75d09c6815
+ <+>
2023-03-24 00:21:36 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_response
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
+  before_action :authorize
+
+  private
+
+  def record_invalid_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def record_not_found_response
+    render json: { error: "#{controller_name.classify} not found" },status: :not_found
+  end
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 end
